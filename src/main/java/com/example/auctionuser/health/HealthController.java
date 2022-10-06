92b9a7b2768f6b9fb9483b1fac6240dda0b131dc
dpound <dpound@naver.com>
2022-10-06 17:26:30 +0900

    레빗엠큐를 이용한 스프링 버스 시스템 구축 완료 테스트 완료

--- src/main/java/com/example/auctionuser/health/HealthController.java
+++ src/main/java/com/example/auctionuser/health/HealthController.java
@@ -1,12 +1,18 @@
 package com.example.auctionuser.health;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "health")
 public class HealthController {
+
+    private final Environment env;
 
     @GetMapping(value = "hello")
     public String getHello(){
@@ -16,6 +22,11 @@ public class HealthController {
     @GetMapping(value = "service-check")
     public String getHealth(){
         return "Service is good";
+    }
+
+    @GetMapping(value = "service-yml")
+    public String getYml(){
+        return env.getProperty("commonTestText");
     }
 
 }
