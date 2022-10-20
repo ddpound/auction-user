5e66eb5369dd38f826ae99b76fe99d07bb2a0a0a
dpound <dpound@naver.com>
2022-10-20 21:38:16 +0900

    현재 유저에는 각각의 판매자와 유저의 권한을 주는 기능을 만들기로했음 판매자 서비스라도 판매자의 권한을 주는 것이라면 여기 user 서비스에서 진행하도록 바꿈

---
+++ src/main/java/com/example/auctionuser/seller/SellerController.java
@@ -0,0 +1,46 @@
+package com.example.auctionuser.seller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping(value = "seller")
+@RestController
+public class SellerController {
+
+    private final SellerService sellerService;
+
+    @PostMapping("give-seller")
+    public ResponseEntity giveSeller(HttpServletRequest request, @RequestBody Map<String,Object> coupon){
+
+        int resultNum = sellerService
+                .sellerRegister(
+                        request,
+                        coupon.get("id").toString(),
+                        coupon.get("code").toString());
+        if(resultNum == 1 ){
+            return new ResponseEntity(HttpStatus.OK);
+        } else if (resultNum == -1) {
+            return new ResponseEntity("JCODE001", HttpStatus.BAD_REQUEST);
+        } else if (resultNum == -3) {
+            return new ResponseEntity("JCODE010", HttpStatus.BAD_REQUEST);
+        }else if (resultNum == -5) {
+            return new ResponseEntity("JCODE800", HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity("It's a coupon I've already used", HttpStatus.FORBIDDEN);
+        }
+
+
+    }
+
+}
---
+++ src/main/java/com/example/auctionuser/seller/SellerService.java
@@ -0,0 +1,68 @@
+package com.example.auctionuser.seller;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.auctionuser.model.SellerCoupon;
+import com.example.auctionuser.model.UserModel;
+import com.example.auctionuser.repository.SellerCouponRepository;
+import com.example.auctionuser.repository.UserModelRepository;
+import com.example.modulecommon.jwtutil.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class SellerService {
+
+    private final JWTUtil jwtUtil;
+
+    private final UserModelRepository userModelRepository;
+
+    private final SellerCouponRepository sellerCouponRepository;
+
+    @Transactional
+    public int sellerRegister(HttpServletRequest request, String id, String code){
+
+        String jwtHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String token = jwtHeader.replace("Bearer ", "");
+
+        DecodedJWT decodedJWT = jwtUtil.getTokenRole(token);
+
+        System.out.println(decodedJWT.getClaim("username").asString());
+
+        UserModel userModel = userModelRepository.findByUsername(decodedJWT.getClaim("username").asString());
+
+        SellerCoupon findSellerCoupon = null;
+        try {
+            findSellerCoupon = sellerCouponRepository.findByIdAndCouponPassword(Integer.parseInt(id),code);
+        }catch (NumberFormatException e){
+            return -5; // id가 String이 아닐때
+        }
+
+
+        // 키값이 날라오지 않음
+        if(id.equals("") && code.equals("")){
+            return -3;
+        }
+
+        // 등록된쿠폰 아님
+        if(findSellerCoupon ==null){
+            return -1;
+        }
+
+        // 만약 이미 등록된 쿠폰 이라는 뜻
+        if(findSellerCoupon.getUserModel() != null){
+            return -2;
+        }
+
+        // 새쿠폰이라면 그럼 쿠폰 등록자를 해주자
+        findSellerCoupon.setUserModel(userModel);
+        userModel.setRoles("ROLE_USER,ROLE_SELLER");
+        return 1;
+    }
+}
